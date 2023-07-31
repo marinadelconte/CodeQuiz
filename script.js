2a607b4c7f33b7f73ccaf1a9df3e85ee88279d11
Marina Del Conte <marinadelconte@yahoo.com>
2023-07-31 00:37:28 -0400

    added additional style features

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+let timerEl = document.querySelector(".time");
+let mainEl = document.getElementById("main");
+let startEl = document.querySelector("#start");
+
 let quizDiv = document.querySelector("#quiz");
 let questionButton1 = document.querySelector("#answer1");
 let highScores = JSON.parse(localStorage.getItem("highScores")) || []
 
+startEl.addEventListener("click", setInterval);
 
 let timer = 90;
 
 let timerId = setInterval(function(){
     timer -= 1;
+    timerEl.textContent = ("Time remaining: " + timer);
     console.log(timer);
 
 
@@ -17,11 +23,11 @@ if (timer === 0) {
 
 }, 1000)
 
-let questions = [{question: "JavaScript and Java are the same coding language", answers: ["True", "False"], correctAnswer: "False"},
-                {question: "What datatype do we use to return the values true or false?", answers: ["Boolean", "String", "Constant", "Null"], correctAnswer: "Boolean"},
-                {question: "What would you use to store multiple variables under a single name?", answers: ["Train", "Array", "Index", "Copy"], correctAnswer: "Array"},
-                {question: "What would we use to check the equality of two variables?", answers: ["=", "&&", "==="], correctAnswer: "==="},
-                {question: "Which of the following is used to alter the length of an array?", answers: ["push()", "kick()", "jump()", "move()"], correctAnswer: "push()"},
+let questions = [{question: "1. JavaScript and Java are the same coding language", answers: ["True", "False"], correctAnswer: "False"},
+                {question: "2. What datatype do we use to return the values true or false?", answers: ["Boolean", "String", "Constant", "Null"], correctAnswer: "Boolean"},
+                {question: "3. What would you use to store multiple variables under a single name?", answers: ["Train", "Array", "Index", "Copy"], correctAnswer: "Array"},
+                {question: "4. What would we use to check the equality of two variables?", answers: ["=", "&&", "==="], correctAnswer: "==="},
+                {question: "5. Which of the following is used to alter the length of an array?", answers: ["push()", "kick()", "jump()", "move()"], correctAnswer: "push()"},
 ]
 
 let currentQuestion = 0;
